f04e5c6f2fdac1d84b5f0295fec29ae6dc1f297f
Gerd von Egidy <gerd@egidy.de>
2014-07-13 16:30:03 +0200

    add sms notification scripts
    CMake installation and documentation still to do.

---
+++ etc/icinga2/scripts/sms-host-notification.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# send host notifications via short message (SMS)
+# 
+# uses smstools http://smstools3.kekekasvi.com/
+# the icinga command user must be member in the group smstools uses
+
+SMSTOOLS_GROUP=smstools
+SMSTOOLS_OUTGOING_DIR=/var/spool/sms/outgoing
+
+TMPFILE=`mktemp /tmp/smsd_XXXXXX`
+
+echo "To: $USERPAGER" >$TMPFILE
+echo "Alphabet: ISO" >>$TMPFILE
+echo "" >>$TMPFILE
+
+DURATION_MIN=$[$DURATION_SEC/60]
+
+cat <<TEMPLATE | iconv -t ISO-8859-15 >>$TMPFILE
+$NOTIFICATIONTYPE - $HOSTDISPLAYNAME is $HOSTSTATE for ${DURATION_MIN}m
+($HOSTOUTPUT)
+TEMPLATE
+
+chmod 660 $TMPFILE
+chgrp $SMSTOOLS_GROUP $TMPFILE
+
+MAXRENAME=10
+while ! mv -n $TMPFILE $SMSTOOLS_OUTGOING_DIR; do
+	# target file seems to already exist
+
+	if [ $MAXRENAME -le 0 ]; then
+		echo "can't write to target directory"
+		exit 1
+	fi
+
+	NEWTMP=`mktemp /tmp/smsd_XXXXXX`
+	mv -f $TMPFILE $NEWTMP
+	TMPFILE=$NEWTMP
+
+	MAXRENAME=$[$MAXRENAME-1]
+done
+
